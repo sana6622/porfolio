9af6ff9a0197a7b0aa9a961fc97cb845356942c2
sana <sana6622@gmail.com>
2024-04-02 15:07:40 +0800

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Hello, This is Home Page.</h1>
+    <h1>Hello, This is Home Page. 試了好幾次終於成功了</h1>
     <RouterLink to="/">Home</RouterLink> |
     <RouterLink to="/about">About</RouterLink>
     <hr>
